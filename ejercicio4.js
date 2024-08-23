2f4365af31b148c52ba2c8a538af72b40d512f2b
Leo79Jp <leojparodi@gmail.com>
2024-08-22 22:07:19 -0300

    Complete el ejercicio 4

--- ejercicio4.js
+++ ejercicio4.js
@@ -4,4 +4,24 @@
 // Captura el error en un bloque `catch` y muestra un mensaje de error en la consola.
 // Independientemente del resultado, muestra "Operación finalizada" en la consola dentro de un bloque `finally`.
 
+const prompt = require('prompt-sync')({ siging: true})
+primerNumero = parseInt(prompt('Ingrese el primer numero: '))
+segundoNumero = parseInt(prompt('Ingrese el segundo numero: '))
 
+const dividirNumeros = (a, b) => {
+    try {
+      if (b === 0) {
+        throw new Error('No se puede dividir por cero')
+      }
+      const resultado = a / b
+      console.log(`El resultado de la división es: ${resultado}`)
+    } catch (error) {
+      console.log('Error : ' + error.message)
+    } finally {
+      console.log('Operación finalizada')
+    }
+  }
+  
+  dividirNumeros(primerNumero, segundoNumero)
+//   dividirNumeros(10, 0)
+  
